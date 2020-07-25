15ab5098973be9d44d748a2bd86975cb73e1ab54
Thomas Reitmaier <treitmaier@gmail.com>
2020-07-25 17:17:02 +0100

    more elaborate example

--- src/main/kotlin/xyz/reitmaier/kinputevents/KInputEventsDemo.kt
+++ src/main/kotlin/xyz/reitmaier/kinputevents/KInputEventsDemo.kt
@@ -1,5 +1,7 @@
 package xyz.reitmaier.kinputevents
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 fun main() {
     // Initialise with keyboard device
     // The input device path on your system, will likely be different (see below)
@@ -15,19 +17,32 @@ fun main() {
      ... and log out and login again.
      */
 
+    var lastKeyPressed = -1
+    val running = AtomicBoolean(true)
     // Add Input Event Listener
     val keyEventListener: OnInputEventListener = {
         // Only listen for key releases and print them out
-        if(it.type == EV_KEY && it.value == EV_KEY_RELEASE)
+        if(it.type == EV_KEY && it.value == EV_KEY_RELEASE) {
             println(it)
+            if(lastKeyPressed == it.code) {
+                running.set(false)
+                println("Exiting")
+            } else {
+                lastKeyPressed = it.code
+            }
+        }
     }
     keyboard.addListener(keyEventListener)
 
     // Start listening for input events
     keyboard.start()
 
-    //Exit after 10 seconds
-    Thread.sleep(10_000L)
+    println("Press any key twice to exit...")
+
+    //Exit when same key is pressed twice
+    while(running.get()) {
+        Thread.sleep(200)
+    }
     keyboard.removeListener(keyEventListener)
     keyboard.exit()
 }
