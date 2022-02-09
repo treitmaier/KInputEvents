79bee7ab7891690770657166aef81dcaf7753e2d
Thomas Reitmaier <thomas@reitmaier.xyz>
2022-02-09 15:15:09 +0000

    formatting + callback interface

--- src/main/kotlin/xyz/reitmaier/kinputevents/KInputEvents.kt
+++ src/main/kotlin/xyz/reitmaier/kinputevents/KInputEvents.kt
@@ -1,6 +1,5 @@
 package xyz.reitmaier.kinputevents
 
-import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.channels.AsynchronousCloseException
@@ -11,113 +10,125 @@ import java.nio.file.StandardOpenOption
 import java.util.concurrent.atomic.AtomicBoolean
 
 
-typealias OnInputEventListener = (event: InputEvent) -> Unit
+//typealias OnInputEventListener = (event: InputEvent) -> Unit
+interface KInputEventListener {
+  fun onEvent(event: InputEvent)
+  fun onException(exception: Exception)
+  fun onExit()
+}
 
 class KInputEvents(private val inputDevicePath: String) {
-    private var fileChannel: ReadableByteChannel? = null
-    private val running = AtomicBoolean(false)
-    private var readerThread: Thread? = null
-    private var listeners: ArrayList<OnInputEventListener> = ArrayList()
+  private var fileChannel: ReadableByteChannel? = null
+  private val running = AtomicBoolean(false)
+  private var readerThread: Thread? = null
+  private var listeners: ArrayList<KInputEventListener> = ArrayList()
 
-    private val is64Bit = System.getProperty("os.arch").contains("64")
+  private val is64Bit = System.getProperty("os.arch").contains("64")
 
-    /*
-    Input Event Layout
-    See Linux Kernel Event Interface
-    https://www.kernel.org/doc/Documentation/input/input.txt
-     */
-    private val inputEventSize = if(is64Bit) 24 else 16
-    private val inputEventData = ByteBuffer.allocate(inputEventSize)
-    private val secondsIndex = 0
-    private val microSecondsIndex = if(is64Bit) 8 else 4
-    private val typeIndex = if(is64Bit) 16 else 8
-    private val codeIndex = if(is64Bit) 18 else 10
-    private val valueIndex = if(is64Bit) 20 else 12
+  /*
+  Input Event Layout
+  See Linux Kernel Event Interface
+  https://www.kernel.org/doc/Documentation/input/input.txt
+   */
+  private val inputEventSize = if(is64Bit) 24 else 16
+  private val inputEventData = ByteBuffer.allocate(inputEventSize)
+  private val secondsIndex = 0
+  private val microSecondsIndex = if(is64Bit) 8 else 4
+  private val typeIndex = if(is64Bit) 16 else 8
+  private val codeIndex = if(is64Bit) 18 else 10
+  private val valueIndex = if(is64Bit) 20 else 12
 
-    fun start() {
-        // Check if reader thread is alive
-        if(readerThread?.isAlive == true) {
-            throw Exception("${this.javaClass.simpleName} is already started.")
-        }
-        // (Close existing and) create new FileChannel
-        fileChannel?.close()
-        fileChannel = FileChannel.open(Paths.get(inputDevicePath), StandardOpenOption.READ)
-
-        // Create & start reader thread
-        running.set(true)
-        readerThread = Thread { poll() }
-        readerThread?.isDaemon = true
-        readerThread?.start()
+  fun start() {
+    // Check if reader thread is alive
+    if(readerThread?.isAlive == true) {
+      throw Exception("${this.javaClass.simpleName} is already started.")
     }
+    // (Close existing and) create new FileChannel
+    fileChannel?.close()
+    fileChannel = FileChannel.open(Paths.get(inputDevicePath), StandardOpenOption.READ)
 
-    fun exit() {
-        running.set(false)
-        try {
-            //Interrupt the reader thread
-            println("Closing Channel")
-            fileChannel?.close()
-//            readerThread?.join()
-        } catch (e: InterruptedException) {
-            e.printStackTrace()
-        }
+    // Create & start reader thread
+    running.set(true)
+    readerThread = Thread { poll() }
+    readerThread?.isDaemon = true
+    readerThread?.start()
+  }
+
+  fun exit() {
+    running.set(false)
+    try {
+      //Interrupt the reader thread
+      fileChannel?.close()
+
+    } catch (e: InterruptedException) {
+      e.printStackTrace()
     }
+  }
 
-    private fun poll() {
-        try {
-            while (running.get()) {
-                // read input event Data
-                fileChannel?.read(inputEventData)
+  private fun poll() {
+    try {
+      while (running.get()) {
+        // read input event Data
+        fileChannel?.read(inputEventData)
 
-                // parse input event data
-                inputEventData.order(ByteOrder.LITTLE_ENDIAN)
-                val seconds: Number = if (is64Bit)
-                    inputEventData.getLong(secondsIndex) else inputEventData.getInt(secondsIndex)
-                val microSeconds: Number = if (is64Bit)
-                    inputEventData.getLong(microSecondsIndex) else inputEventData.getInt(microSecondsIndex)
-                val timestamp = "$seconds.$microSeconds"
+        // parse input event data
+        inputEventData.order(ByteOrder.LITTLE_ENDIAN)
+        val seconds: Number = if (is64Bit)
+          inputEventData.getLong(secondsIndex) else inputEventData.getInt(secondsIndex)
+        val microSeconds: Number = if (is64Bit)
+          inputEventData.getLong(microSecondsIndex) else inputEventData.getInt(microSecondsIndex)
+        val timestamp = "$seconds.$microSeconds"
 
-                val type = inputEventData.getShort(typeIndex).toInt()
-                val code = inputEventData.getShort(codeIndex).toInt()
-                val value = inputEventData.getInt(valueIndex)
+        val type = inputEventData.getShort(typeIndex).toInt()
+        val code = inputEventData.getShort(codeIndex).toInt()
+        val value = inputEventData.getInt(valueIndex)
 
-                //reset the position to zero
-                inputEventData.flip()
+        //reset the position to zero
+        inputEventData.flip()
 
-                // distribute the event to listeners
-                synchronized(listeners) {
-                    listeners.forEach { it(InputEvent(timestamp, type, code, value)) }
-                }
-            }
-        } catch (e: AsynchronousCloseException) {
-            // FileChannel was closed by exit() function
-            // Do nothing
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-
-    fun addListener(listener: OnInputEventListener) {
+        // distribute the event to listeners
         synchronized(listeners) {
-            listeners.add(listener)
+          listeners.forEach { it.onEvent(InputEvent(timestamp, type, code, value)) }
         }
+      }
+    } catch (e: AsynchronousCloseException) {
+      // FileChannel was closed by exit() function
+      // Do nothing
+      synchronized(listeners) {
+        listeners.forEach { it.onExit() }
+      }
+    } catch (e: Exception) {
+      synchronized(listeners) {
+        listeners.forEach {
+          it.onException(e)
+          it.onExit()
+        }
+      }
     }
+  }
 
-    fun removeListener(listener: OnInputEventListener) {
-        synchronized(listeners) {
-            listeners.remove(listener)
-        }
+
+  fun addListener(listener: KInputEventListener) {
+    synchronized(listeners) {
+      listeners.add(listener)
     }
+  }
+
+  fun removeListener(listener: KInputEventListener) {
+    synchronized(listeners) {
+      listeners.remove(listener)
+    }
+  }
 
 }
 
 data class InputEvent(val timestamp: String, val type: Int, val code: Int, val value: Int) {
-    override fun toString() =
-            when (type) {
-                EV_KEY -> "Event: Time $timestamp," +
-                        " type $type (${EVENT_TYPES[type].orEmpty()})," +
-                        " code $code (${KEYCODES[code].orEmpty()})," +
-                        " value $value (${EV_KEY_VALUES[value].orEmpty()})"
-                else -> "Event: Time $timestamp, type $type , code $code, value $value"
-            }
+  override fun toString() =
+    when (type) {
+      EV_KEY -> "Event: Time $timestamp," +
+        " type $type (${EVENT_TYPES[type].orEmpty()})," +
+        " code $code (${KEYCODES[code].orEmpty()})," +
+        " value $value (${EV_KEY_VALUES[value].orEmpty()})"
+      else -> "Event: Time $timestamp, type $type , code $code, value $value"
+    }
 }
--- src/main/kotlin/xyz/reitmaier/kinputevents/KInputEventsDemo.kt
+++ src/main/kotlin/xyz/reitmaier/kinputevents/KInputEventsDemo.kt
@@ -3,46 +3,55 @@ package xyz.reitmaier.kinputevents
 import java.util.concurrent.atomic.AtomicBoolean
 
 fun main() {
-    // Initialise with keyboard device
-    // The input device path on your system, will likely be different (see below)
-    val keyboard = KInputEvents("/dev/input/event0")
-    /*
-     to list input devices run
-     evtest
+  // Initialise with keyboard device
+  // The input device path on your system, will likely be different (see below)
+  val keyboard = KInputEvents("/dev/input/event0")
+  /*
+   to list input devices run
+   evtest
 
-     Also ensure current user has permission to access input device.
-     On PiOS/Raspbian the easiest way is to add $USER to input group:
-     sudo usermod -a -G input $USER
+   Also ensure current user has permission to access input device.
+   On PiOS/Raspbian the easiest way is to add $USER to input group:
+   sudo usermod -a -G input $USER
 
-     ... and log out and login again.
-     */
+   ... and log out and login again.
+   */
 
-    var lastKeyPressed = -1
-    val running = AtomicBoolean(true)
-    // Add Input Event Listener
-    val keyEventListener: OnInputEventListener = {
-        // Only listen for key releases and print them out
-        if(it.type == EV_KEY && it.value == EV_KEY_RELEASE) {
-            println(it)
-            if(lastKeyPressed == it.code) {
-                running.set(false)
-                println("Exiting")
-            } else {
-                lastKeyPressed = it.code
-            }
+  var lastKeyPressed = -1
+  val running = AtomicBoolean(true)
+  // Add Input Event Listener
+  val listener= object : KInputEventListener {
+    override fun onEvent(event: InputEvent) {
+      if(event.type == EV_KEY && event.value == EV_KEY_RELEASE) {
+        println(event)
+        if(lastKeyPressed == event.code) {
+          running.set(false)
+        } else {
+          lastKeyPressed = event.code
         }
+      }
     }
-    keyboard.addListener(keyEventListener)
 
-    // Start listening for input events
-    keyboard.start()
-
-    println("Press any key twice to exit...")
-
-    //Exit when same key is pressed twice
-    while(running.get()) {
-        Thread.sleep(200)
+    override fun onException(exception: Exception) {
+      exception.printStackTrace()
     }
-    keyboard.removeListener(keyEventListener)
-    keyboard.exit()
+
+    override fun onExit() {
+      println("Exiting")
+    }
+
+  }
+  keyboard.addListener(listener)
+
+  // Start listening for input events
+  keyboard.start()
+
+  println("Press any key twice to exit...")
+
+  //Exit when same key is pressed twice
+  while(running.get()) {
+    Thread.sleep(200)
+  }
+  keyboard.exit()
+  keyboard.removeListener(listener)
 }
